bd1c452393db99e08662f4d36aaa3bf923e32677
y2sud <esudeep@outlook.com>
2016-03-03 22:01:09 +0530

    Update readdb.py
    V2.4

--- readdb.py
+++ readdb.py
@@ -7,16 +7,23 @@ import os
 
 print 'hello 2'
 
-DB_IP = os.environ.get('OPENSHIFT_MYSQL_DB_HOST','127.0.0.1')
-DB_PORT = int(os.environ.get('OPENSHIFT_MYSQL_DB_PORT',3306))
-DB_USER = os.environ.get('OPENSHIFT_MYSQL_DB_USERNAME','sudeep')
-DB_PWD = os.environ.get('OPENSHIFT_MYSQL_DB_PASSWORD','Hello_AWS')
+DB_IP = os.environ.get('EXTERNAL_MYSQL_SERVICE_SERVICE_HOST','127.0.0.1')
+DB_IP2 = os.environ.get('MYSQL_SERVICE_HOST','127.0.0.1')
+
+DB_PORT = int(os.environ.get('EXTERNAL_MYSQL_SERVICE_SERVICE_PORT',3306))
+DB_USER = os.environ.get('MYSQL_USER','sudeep')
+DB_PWD = os.environ.get('MYSQL_PASSWORD','Hello_AWS')
+DB_NAME = os.environ.get('MYSQL_DATABASE','Hello_AWS')
+
+print 'db_ip ' + DB_IP
+print 'db_ip2 ' + DB_IP2
+print 'db_name ' + DB_NAME
 
 conn = MySQLdb.connect(host=DB_IP,    # your host, usually localhost
-					 port=DB_PORT,
+			port=DB_PORT,
                      user=DB_USER,         # your username
                      passwd=DB_PWD,  # your password
-                     db="db01")        # name of the data base
+                     db=DB_NAME)        # name of the data base
 
 # you must create a Cursor object. It will let
 #  you execute all the queries you need
