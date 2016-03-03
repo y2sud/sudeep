a81186348179cccb41f75bfa3b67e417b2f12a9d
y2sud <esudeep@outlook.com>
2016-03-03 22:14:00 +0530

    Update readdb.py
    V2.6

--- readdb.py
+++ readdb.py
@@ -2,7 +2,7 @@
 
 print 'hello 1'
 
-#import MySQLdb
+
 import os
 
 print 'hello 2'
@@ -10,15 +10,16 @@ print 'hello 2'
 DB_IP = os.environ.get('EXTERNAL_MYSQL_SERVICE_SERVICE_HOST','127.0.0.1')
 DB_IP2 = os.environ.get('MYSQL_SERVICE_HOST','127.0.0.1')
 
-DB_PORT = int(os.environ.get('EXTERNAL_MYSQL_SERVICE_SERVICE_PORT',3306))
-DB_USER = os.environ.get('MYSQL_USER','sudeep')
-DB_PWD = os.environ.get('MYSQL_PASSWORD','Hello_AWS')
-DB_NAME = os.environ.get('MYSQL_DATABASE','???')
+DB_PORT = int(os.environ.get('MYSQL_SERVICE_PORT',3306))
+DB_USER = os.environ.get('DATABASE_USER','sudeep')
+DB_PWD = os.environ.get('DATABASE_PASSWORD','Hello_AWS')
+DB_NAME = os.environ.get('DATABASE_NAME','???')
 
 print 'db_ip ' + DB_IP
 print 'db_ip2 ' + DB_IP2
 print 'db_name ' + DB_NAME
 
+import MySQLdb
 conn = MySQLdb.connect(host=DB_IP,    # your host, usually localhost
 			port=DB_PORT,
                      user=DB_USER,         # your username
